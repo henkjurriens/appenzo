fd555ebc6879a9261bd16b7c579eccf6707f16b2
Henk Jurriens <henk.jurriens@gmail.com>
2015-10-13 12:09:59 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appenzoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appenzoApp
+ */
+angular.module('appenzoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appenzoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appenzoApp
+ */
+angular.module('appenzoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
